5057ac1356ea7c16d675768ce19180827b8321f5
Omar Eldash <omareddash@gmail.com>
2019-04-07 15:54:50 -0500

    Update generatingProgMemData.py

--- generatingProgMemData.py
+++ generatingProgMemData.py
@@ -8,10 +8,15 @@
 import pandas as pd
 import numpy
 
+#pmdata is the file where the final output is written, to be later copied into CPP code
 pmdata = open("progmemdata.txt","w");
 
+# reading image file, at the moment it's done from a csv which can be easily replaced with any image read in python
 image1 = pd.read_csv("Image4.csv", index_col=None, header=None);
+#transforming it into numpy array
 img = numpy.array(image1);
+
+#nested loops to generate data in the form of 2D values of m x n size: {{,,,},{,,,},{,,,},{,,,]}
 pmdata.write('{')
 d1,d2 = img.shape
 for i in range(d1):
